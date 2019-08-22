e24e51ebbac51d6161a73cae8198dc943def6720
lvsuzhen <1205521940@qq.com>
2019-08-22 17:38:01 +0800

    解决import的一个bug

---
+++ lib/__tests__/button.unit.tsx
@@ -0,0 +1,9 @@
+import renderer from 'react-test-renderer'
+import React from 'react'
+import Button from '../button'
+describe('button',()=>{
+    it('它是个button',()=>{
+        const json = renderer.create(<Button/>).toJSON()
+        expect(json).toMatchSnapshot()
+    })
+})
