c973762a7a8eeb09b4fa09624f5a8ba94094ebc5
lvsuzhen <1205521940@qq.com>
2019-08-22 12:44:02 +0800

    项目基础配置

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var { CleanWebpackPlugin } = require('clean-webpack-plugin');
+var HtmlWebapckPlugin = require('html-webpack-plugin')
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname,'dist/lib'),
+        library: 'reactui',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.tsx?$/ ,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+}
