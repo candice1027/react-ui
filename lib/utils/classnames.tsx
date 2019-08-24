b01913bfc16027ea4f26a2b8bbcded2f445da641
lvsuzhen <1205521940@qq.com>
2019-08-24 11:45:46 +0800

    更高

--- lib/utils/classnames.tsx
+++ lib/utils/classnames.tsx
@@ -1,8 +1,6 @@
 export default function classnames(...names: (string | undefined)[]) {
     let newName = names.filter(Boolean);
     // let newNameArr = []
-    // for (let item of newName) {
-    //     newNameArr.push(item.replace(/\s+/g,''))
-    // } 
+    
     return newName.join(' ')
 }
