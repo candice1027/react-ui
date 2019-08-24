9f0369b08067075ef99575ebf80e89607e5bf028
lvsuzhen <1205521940@qq.com>
2019-08-24 19:48:52 +0800

    增加dialog组件

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import IconExample from '../lib/icon/icon.example';
 import ButtonExample from '../lib/button/button.example';
+import DialogExample from '../lib/dialog/dialog.example';
 import { Route } from 'react-router-dom'
 
 export default function Main() {
@@ -8,6 +9,7 @@ export default function Main() {
         <main>
             <Route path="/icon" component={IconExample}></Route> 
             <Route path="/button" component={ButtonExample}></Route> 
+            <Route path="/dialog" component={DialogExample}></Route> 
         </main>
     )
 }
