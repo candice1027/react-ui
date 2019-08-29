def7399efb4e9252ac969e11e263825c1c457257
lvsuzhen <1205521940@qq.com>
2019-08-29 12:47:05 +0800

    渲染表单

--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -2,23 +2,26 @@ import React, { ReactFragment } from 'react'
 interface Props {
     value: {[key:string]:any};
     fields: Array<{name:string,label:string,input:{type:string}}>;
-    buttons: ReactFragment
+    buttons: ReactFragment;
 }
 const Form:React.FunctionComponent<Props> = (props) =>{
     return (
-     <form>
-        {
-            props.fields.map((item,key) =>
-                <div key={key}>
-                    {item.label}
-                    <input type={item.input.type} placeholder={props.value[item.name]}/>
-                </div>
-            )
-        }
-        <div>
-            {props.buttons}
-        </div> 
-     </form>
+        <form>
+            {
+                props.fields.map(item =>{
+                        return (
+                            <div key={item.name}>
+                                {item.label}
+                                <input type={item.input.type} placeholder={props.value[item.name]}/>
+                            </div>
+                            )
+                    }
+                )
+            }
+            <div>
+                {props.buttons}
+            </div> 
+        </form>
     )
 }
 export default Form
