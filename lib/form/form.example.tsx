1aa856a1fff666d7d08587a14ffc88e3d0dd8247
lvsuzhen <1205521940@qq.com>
2019-08-29 12:30:27 +0800

    定义表单的api并初步渲染

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React,{useState, Fragment} from 'react'
 import Form from './form'
-const FormExample = () =>{
+const FormExample:React.FunctionComponent = () =>{
+    const [formData] = useState({
+        username:'请输入用户名',
+        password: '请输入密码',
+        telephone:'请输入手机号'
+    })
+    const [fields] = useState([
+        {name: 'username', label: '用户名', input: {type:'text'}},
+        {name: 'password', label: '密码', input: {type:'password'}},
+        {name: 'telephone', label: '手机号', input: {type:'tel'}}
+
+    ])
     return (
-        <div>
-            <Form />
-        </div>
+        <Form value={formData} fields={fields} buttons={
+            <Fragment>
+                <button type="submit">提交</button>
+                <button >返回</button>
+            </Fragment>
+        }>   
+        </Form>
     )
 }
 export default FormExample
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-
-const Form:React.FunctionComponent = () =>{
+import React, { ReactFragment } from 'react'
+interface Props {
+    value: {[key:string]:any};
+    fields: Array<{name:string,label:string,input:{type:string}}>;
+    buttons: ReactFragment
+}
+const Form:React.FunctionComponent<Props> = (props) =>{
     return (
-     <div>
-         this is form 
-     </div>
+     <form>
+        {
+            props.fields.map((item,key) =>
+                <div key={key}>
+                    {item.label}
+                    <input type={item.input.type} placeholder={props.value[item.name]}/>
+                </div>
+            )
+        }
+        <div>
+            {props.buttons}
+        </div> 
+     </form>
     )
 }
 export default Form
