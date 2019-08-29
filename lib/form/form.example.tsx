089fe020be8210736965faa65d1bbe0b26542a99
lvsuzhen <1205521940@qq.com>
2019-08-29 15:14:26 +0800

    表单更新

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,7 +1,8 @@
 import React,{useState, Fragment} from 'react'
 import Form from './form'
+import {FormValue} from './form'
 const FormExample:React.FunctionComponent = () =>{
-    const [formData] = useState({
+    const [formData,setFormData] = useState<FormValue>({
         username:'请输入用户名',
         password: '请输入密码',
         telephone:'请输入手机号'
@@ -12,13 +13,22 @@ const FormExample:React.FunctionComponent = () =>{
         {name: 'telephone', label: '手机号', input: {type:'tel'}}
 
     ])
+    const onSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
+        console.log(formData)
+    }
     return (
-        <Form value={formData} fields={fields} buttons={
-            <Fragment>
-                <button type="submit">提交</button>
-                <button >返回</button>
-            </Fragment>
-        }>   
+        <Form 
+            value={formData} 
+            fields={fields} 
+            buttons={
+                <Fragment>
+                    <button type="submit">提交</button>
+                    <button >返回</button>
+                </Fragment>
+            }
+            onSubmit = {onSubmit}
+            onChange= {(newValue) =>{setFormData(newValue)}}
+        >   
         </Form>
     )
 }
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,18 +1,36 @@
-import React, { ReactFragment } from 'react'
+import React, { ReactFragment, FormEventHandler } from 'react'
+export interface FormValue {
+    [key:string]:any
+}
 interface Props {
-    value: {[key:string]:any};
+    value: FormValue;
     fields: Array<{name:string,label:string,input:{type:string}}>;
     buttons: ReactFragment;
+    onSubmit: FormEventHandler<HTMLFormElement>;
+    onChange: (value: FormValue) => void
 }
 const Form:React.FunctionComponent<Props> = (props) =>{
+    const formData = props.value;
+    const submitData:React.FormEventHandler<HTMLFormElement> = (e)=>{
+        e.preventDefault()
+        props.onSubmit(e)
+    }
+    const onInputChange = (name:string,e: React.ChangeEvent<HTMLInputElement>)=>{
+        const newFormData = {...formData,[name]:e.target.value}
+        props.onChange(newFormData)
+    }
     return (
-        <form>
+        <form onSubmit={submitData}>
             {
                 props.fields.map(item =>{
                         return (
                             <div key={item.name}>
                                 {item.label}
-                                <input type={item.input.type} placeholder={props.value[item.name]}/>
+                                <input 
+                                    type={item.input.type} 
+                                    value={formData[item.name]}
+                                    onChange={onInputChange.bind(null,item.name)}
+                                />
                             </div>
                             )
                     }
